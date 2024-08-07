46721ba47d2d826182b44b6225f115f083d3be8d
Fabien Reumont-Locke <reumontf@gmail.com>
2024-08-06 21:58:08 -0700

    Improved pressed sound.

--- Web/sound/pressed.js
+++ Web/sound/pressed.js
@@ -2,22 +2,19 @@ class PressedSound {
   speedMultiplier = 0;
 
   constructor(masterLimiter) {
-    this.basicGain = new Tone.Gain(1).connect(masterLimiter);
-    this.pressedSynth = new Tone.FMSynth().connect(this.basicGain);
-    this.pressedSynth.volume.value = 0;
+    this.pressedSynth = new Tone.FMSynth().toDestination();
     this.bassSynth = new Tone.MonoSynth({
       oscillator: { type: "sine" },
       envelope: { attack: 0.001, decay: 0, sustain: 1, release: 10 }
-    }).connect(this.basicGain);
-    this.bassSynth.volume.value = 0;
+    }).toDestination();
     this.notesForMultiplier = [["C2", "D4"], ["F2", "G4"], ["Bb3", "C5"], ["Eb3", "F5"]];
     // this.bassOscillator = new Tone.Oscillator("C2", "sine").toDestination();
 
+    this.bassGain = new Tone.Gain(0).toDestination();
     this.bassOscillator = new Tone.Oscillator({
       frequency: "C1",
       type: "sine"
     }).start();
-    this.bassGain = new Tone.Gain(0).connect(masterLimiter);
     this.bassOscillator.connect(this.bassGain);
 
     this.lfo = new Tone.LFO({
@@ -27,7 +24,8 @@ class PressedSound {
     }).start();
     this.lfo.connect(this.bassGain.gain);
 
-    // A soft chime sound that accelerates over time.
+    // A soft chime sound that accelerates over time and becomes more distorted.
+    this.chebyshev = new Tone.Chebyshev(50).toDestination();
     this.acceleratingSynth = new Tone.Synth({
       oscillator: {
         type: 'sine' // Use a sine wave for a soft sound
@@ -36,29 +34,25 @@ class PressedSound {
         attack: 0.001,
         decay: 0.2,
         sustain: 0.1,
-        release: 1.5
+        release: 1.5,
       },
-      volume: -20,
-    }).connect(this.basicGain);
+    }).connect(this.chebyshev);
     this.acceleratingSynthLoop = new Tone.Loop((time) => {
       this.acceleratingSynth.triggerAttackRelease("C6", "8n", time);
-    }, "8n");
+    }, "4n");
   }
 
   play() {
-    this.bassSynth.triggerAttackRelease(this.notesForMultiplier[this.speedMultiplier][0], "4n");
-    this.pressedSynth.triggerAttackRelease(this.notesForMultiplier[this.speedMultiplier][1], "4n");
+    Tone.Transport.bpm.value = 60;
+    this.bassSynth.triggerAttackRelease(this.notesForMultiplier[this.speedMultiplier][0], "8n");
+    this.pressedSynth.triggerAttackRelease(this.notesForMultiplier[this.speedMultiplier][1], "8n");
+
+    this.chebyshev.wet.value = 0;
+    this.acceleratingSynth.detune.value = 0;
     this.acceleratingSynthLoop.start();
-    Tone.getTransport().bpm.value = 60;
-    Tone.getTransport().bpm.rampTo(800, 5);
-
-
-    // this.bassGain.gain.setValueAtTime(1, Tone.now());
-    // this.lfo.frequency.value = this.speedMultiplier;
-    // this.lfo.start();
-    // bassOscillator.frequency.value = 220 * speedMultiplier;  // 220 Hz is A2
-    // bassOscillator.start();
-    // setTimeout(() => bassOscillator.stop(), 500);  // Plays for 500 ms
+    Tone.Transport.bpm.exponentialRampTo(800, 14, "+1");
+    this.acceleratingSynth.detune.exponentialRampTo(1200, 14, "+1");
+    this.chebyshev.wet.rampTo(1, 5, "+10");
   }
 
   pause() {
